2c18817258e3d4bc4f79360e1b234f9588771d11
Michal Terranova <michalterranova@mail.com>
2019-06-27 02:13:30 -0400

    Contains images, and style for game working display buttons for directions only

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+//button to enable the directions to appear and disappear on the screen.
+function rules() {
+    var x = document.getElementById("rules");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function bios() {
+    var x = document.getElementById("collection");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  
